82960adb2a9fa32a915285492c97695c1251ce28
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:57:40 +0300

    Unified plugins deployment

--- Plugin.ApkImageView/Properties/AssemblyInfo.cs
+++ Plugin.ApkImageView/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("ca0bd89a-318f-4fa3-9a5e-49b3e1358a53")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,15 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=111")]
 #else
 
-[assembly: AssemblyTitle("Plugin.ApkImageView")]
 [assembly: AssemblyDescription("Android (.APK) Package viewer")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.ApkImageView")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
 #endif
 
